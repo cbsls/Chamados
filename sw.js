10eccd0ef6e4800762af14b0d2386c42a2e053ee
saymonborges <saymmon@gmail.com>
2026-05-03 19:54:10 -0300

    Replace icon files with a single icon.png

--- sw.js
+++ sw.js
@@ -4,8 +4,7 @@ const FILES_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  './icon.png'
 ]
 
 // 🔹 INSTALAÇÃO (CORRIGIDO - NÃO QUEBRA SE FALTAR ARQUIVO)
